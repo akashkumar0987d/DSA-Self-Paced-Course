f787be25f2718916e2f6b940c4f65af6394aca48
Akash Kumar <80807783+akashkumar0987d@users.noreply.github.com>
2022-01-05 02:51:02 +0530

    Create 12_lcm_of_two_number_efficent_solution.cpp

---
+++ 02. Mathematics/12_lcm_of_two_number_efficent_solution.cpp
@@ -0,0 +1,19 @@
+// Efficient solution to find lcm of 2 numbers
+
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a,int b){
+    if(b==0)
+    return a;
+    return gcd(b,a%b);
+}
+int lcm(int a,int b){
+    return (a*b)/gcd(a,b);
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<lcm(a,b);
+    return 0;
+}
