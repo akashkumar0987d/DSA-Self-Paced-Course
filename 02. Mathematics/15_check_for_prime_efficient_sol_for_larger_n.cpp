9dcb2e6eb6cd5eb69ed75446243d45946dc62978
Akash Kumar <80807783+akashkumar0987d@users.noreply.github.com>
2022-01-05 03:02:43 +0530

    Create 15_check_for_prime_efficient_sol_for_larger_n.cpp

---
+++ 02. Mathematics/15_check_for_prime_efficient_sol_for_larger_n.cpp
@@ -0,0 +1,27 @@
+// Efficient solution to check if the larger number is prime or not.
+
+#include<bits/stdc++.h>
+using namespace std;
+bool isprime(int n){
+    if(n==1)
+    return false;
+    if(n%2==0||n%3==0) 
+    return false;
+    for(int i=5;i*i<=n;i=i+6)
+    {
+        if(n%i==0||n%(i+2)==0)
+        return false;
+    }
+    return true;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    if(isprime(n)){
+        cout<<n<<" is a prime number.";
+    }
+    else
+    cout<<n<<" is not a prime number.";
+    return 0;
+}
