0266ab8005817104c131db43091dd6af6e3125f0
Akash Kumar <80807783+akashkumar0987d@users.noreply.github.com>
2022-01-05 03:34:11 +0530

    Create 22_print_all_prime_smaller_than_n.cpp

---
+++ 02. Mathematics/22_print_all_prime_smaller_than_n.cpp
@@ -0,0 +1,32 @@
+// Naive solution to print all the prime number smaller than or equal to n
+
+#include<bits/stdc++.h>
+using namespace std;
+bool isprime(int n){
+    if(n==1)
+    return false;
+    if(n==2||n==3)
+    return true;
+    if(n%2==0||n%3==0) 
+    return false;
+    for(int i=5;i*i<=n;i=i+6)
+    {
+        if(n%i==0||n%(i+2)==0)
+        return false;
+    }
+    return true;
+}
+void printprime(int n){
+    for(int i=2;i<=n;i++){
+        if(isprime(i)){
+            cout<<i<<" ";
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    printprime(n);
+    return 0;
+}
