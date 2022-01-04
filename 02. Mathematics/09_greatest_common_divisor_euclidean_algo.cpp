eeacc8b17ff790abf4795eb109f46b96ab0ca81c
Akash Kumar <80807783+akashkumar0987d@users.noreply.github.com>
2022-01-05 02:41:35 +0530

    Create 09_greatest_common_divisor_euclidean_algo.cpp

---
+++ 02. Mathematics/09_greatest_common_divisor_euclidean_algo.cpp
@@ -0,0 +1,20 @@
+// Euclidean alogrithm to find the gcd of two number
+
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a,int b){
+    while(a!=b){
+        if(a>b)
+        a=a-b;
+        else
+        b=b-a;
+    }
+    return a;
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<gcd(a,b);
+    return 0;
+}
