a9a28e42e3dcfd3b645721c501de89864f7dadd0
Akash Kumar <80807783+akashkumar0987d@users.noreply.github.com>
2022-01-05 02:54:22 +0530

    Create 13_check_for_prime.cpp

---
+++ 02. Mathematics/13_check_for_prime.cpp
@@ -0,0 +1,27 @@
+// Naive solution to check if a given number is prime or not
+
+#include<bits/stdc++.h>
+using namespace std;
+bool isprime(int n){
+    if(n==1)
+    return false;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    if(isprime(n)){
+        cout<<n<<" is a prime number.";
+    }
+    else
+    cout<<n<<" is not a prime number.";
+    return 0;
+}
