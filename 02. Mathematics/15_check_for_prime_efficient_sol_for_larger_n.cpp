0cda522e79cd2cbcdad0a87e7aedc8e28db80b48
Akash Kumar <80807783+akashkumar0987d@users.noreply.github.com>
2022-01-05 03:09:39 +0530

    Update 15_check_for_prime_efficient_sol_for_larger_n.cpp

--- 02. Mathematics/15_check_for_prime_efficient_sol_for_larger_n.cpp
+++ 02. Mathematics/15_check_for_prime_efficient_sol_for_larger_n.cpp
@@ -5,6 +5,8 @@ using namespace std;
 bool isprime(int n){
     if(n==1)
     return false;
+    if(n==2||n==3)
+    return true;
     if(n%2==0||n%3==0) 
     return false;
     for(int i=5;i*i<=n;i=i+6)
