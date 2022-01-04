c4dde09e91b9984e2f3419b3bab6a0d1d881a690
Akash Kumar <80807783+akashkumar0987d@users.noreply.github.com>
2022-01-05 04:01:54 +0530

    Create 26_Computation_of_power_efficient_sol.cpp

---
+++ 02. Mathematics/26_Computation_of_power_efficient_sol.cpp
@@ -0,0 +1,21 @@
+// Efficient solution to find power
+
+#include<bits/stdc++.h>
+using namespace std;
+int power(int x,int n){
+    if(n==0)
+    return 1;
+    int temp=power(x,n/2);
+    temp=temp*temp;
+    if(n%2==0)
+    return temp;
+    else
+    return temp*x;
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<power(a,b);
+    return 0;
+}
