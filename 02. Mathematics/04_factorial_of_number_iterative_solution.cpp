60439a407fd4cf996ee1a4f4db38d23cf5bc8b11
Akash Kumar <80807783+akashkumar0987d@users.noreply.github.com>
2022-01-05 02:24:43 +0530

    Create 04_factorial_of_number_iterative_solution.cpp

---
+++ 02. Mathematics/04_factorial_of_number_iterative_solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long int fact(int n){
+    long long int res=1;
+    for(int i=2;i<=n;i++)
+    res*=i;
+    return res;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<fact(n);
+    return 0;
+}
