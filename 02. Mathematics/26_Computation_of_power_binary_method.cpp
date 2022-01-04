04409004f6376900355cdd1aed8332b95544c628
Akash Kumar <80807783+akashkumar0987d@users.noreply.github.com>
2022-01-05 04:07:51 +0530

    Create 26_Computation_of_power_binary_method.cpp

---
+++ 02. Mathematics/26_Computation_of_power_binary_method.cpp
@@ -0,0 +1,21 @@
+// Binary exponentiation method to find the power
+
+#include<bits/stdc++.h>
+using namespace std;
+int power(int x,int n){
+    int res=1;
+    while(n>0){
+        if(n%2!=0)
+        res=res*x;
+        x=x*x;
+        n=n/2;
+    }
+    return res;
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<power(a,b);
+    return 0;
+}
