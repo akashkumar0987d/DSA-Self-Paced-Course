808d42048a6f762b77df205e9c0a858899ec6f00
Akash Kumar <80807783+akashkumar0987d@users.noreply.github.com>
2022-01-05 02:45:09 +0530

    Create 10_greatest_common_divisor_euclidean_algo_optimized.cpp

---
+++ 02. Mathematics/10_greatest_common_divisor_euclidean_algo_optimized.cpp
@@ -0,0 +1,17 @@
+// Optimized implementation of Euclidean Alogrithm to find gcd of two number
+
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a,int b){
+    if(b==0)
+    return a;
+    else
+    return gcd(b,a%b);
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<gcd(a,b);
+    return 0;
+}
